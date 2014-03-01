ff8821f3733b3209e2cf6e383d12ef1f8727d551
benjammin93 <benjaminrabin@benjamins-mbp.gateway.2wire.net>
2014-03-01 18:48:42 -0500

    created app for project zero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projectzero::Application.config.secret_key_base = '0d6830db7a8658eedbd0d65aa0237839b6301da40345c4cb88827ac6d4e0e018554e68b457c496124570d148f257d3e533e05172596b84f47ff921d51b787b38'
